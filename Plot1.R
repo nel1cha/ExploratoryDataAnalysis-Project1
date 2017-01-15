65b88bd422c6c29cf09cbd759ee7111378b91c06
nel1cha <nchatue@gmail.com>
2017-01-15 06:03:17 +0100

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -1,9 +1,19 @@
+## load the data
 dataFile <- "./data/household_power_consumption.txt"
 data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+
+# Subset the data
 subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 
-#str(subSetData)
+# Convert dates
 globalActivePower <- as.numeric(subSetData$Global_active_power)
-png("plot1.png", width=480, height=480)
-hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+
+# Plot 1
+attach(data)
+hist(Global_active_power, main = "Global Active Power", 
+     xlab = "Global Active Power (kilowatts)", col = "Red")
+
+# Save file
+dev.copy(png, file = "plot1.png", height = 480, width = 480)
 dev.off()
+detach(data)
